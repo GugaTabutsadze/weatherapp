c337b5a7ba12c6ad05beb9a6b75437d53168497d
Guga <g.tabucadze@gmail.com>
2024-12-23 13:50:03 +0400

    update

--- app/components/weatherdata/Weatherdata.tsx
+++ app/components/weatherdata/Weatherdata.tsx
@@ -6,6 +6,11 @@ interface WeatherData {
   name: string;
   main: {
     temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
   };
   weather: {
     description: string;
