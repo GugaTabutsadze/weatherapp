ff897dbe88bf6c31da0941052a71372a1c3922bc
Guga <g.tabucadze@gmail.com>
2024-12-23 13:54:28 +0400

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,11 @@ interface WeatherData {
   name: string;
   main: {
     temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
   };
   weather: {
     description: string;
